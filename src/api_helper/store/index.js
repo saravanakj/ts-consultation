ebcb18e88c6134a4e4cd99407f01e00a3fed8824
Thiru <5789365-WebTechies@users.noreply.gitlab.com>
2020-09-06 10:19:07 +0530

    Api_Call

--- src/api_helper/store/index.js
+++ src/api_helper/store/index.js
@@ -9,7 +9,7 @@ import storage from "redux-persist/lib/storage";
 const persistConfig = {
   key: "root",
   storage: storage,
-  whitelist: ["authentication", "admin","compaines"]
+  whitelist: []
 };
 
 const middleware = () => {
