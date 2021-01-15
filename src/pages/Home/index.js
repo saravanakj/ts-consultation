61a5a20051f07a805f741979b7665c934344c5d2
Nandhakumar <medianandha@gmail.com>
2021-01-15 23:59:02 +0530

    UI Changes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -211,7 +211,7 @@ class Home extends Component {
                     {c.bukthiLord !==undefined&& <td className="highlight">{c.bukthiLord.substring(0,2)}</td>}
                     {c.antharaLord ===undefined&&<td>{""}</td>}
                     {c.antharaLord !==undefined&&<td className="highlight">{c.antharaLord.substring(0,2)}</td>}
-                    <td>{c.degree||""}</td>
+                    <td>{c.degree.toFixed(3)||""}</td>
                     {c.planet ===undefined &&<td className="highlight">{c.planet||""}</td>}
                     {c.planet !==undefined &&<td className="highlight">{c.planet.substring(0,2)}</td>}
                     {c.planetSignLord ===undefined &&<td className="dull_part">{c.planetSignLord||""}</td>}
@@ -229,69 +229,7 @@ class Home extends Component {
 
           </div>
           <div className="pt_table table_split float_right">
-            <table className="table">
-              <thead className="table_header">
-                <tr>
-                  <td>B#</td>
-                  <td>Hidden</td>
-                  <td>SL</td>
-                  <td>DAL</td>
-                  <td>BHL</td>
-                  <td>ANL</td>
-                  <td>F.Degree</td>
-                  <td>PL</td>
-                  <td>PSL</td>
-                  <td>NL</td>
-                  <td>PSUL</td>
-                  <td>SSL</td>
-            
-                </tr>
-              </thead>
-              <tbody>
-                {data.map((c, idx) => (
-                  <tr key={`data${idx}`}>
-                    <td>{c.houseId}</td>
-                  
-                    <td>
-                        <Fragment>
-                          <i class="material-icons"  style={{cursor:'pointer'}}data-tip data-for={`planetSignNametooltip${idx}`}>
-                            visibility_off</i>
-                          <ReactTooltip place="top" type="dark" id={`planetSignNametooltip${idx}`}>
-                            PSN:{c.planetSignName||"-"}&nbsp;,DN:{c.houseNakshathraName||"-"},&nbsp;HSN:{c.houseSignName||"-"},&nbsp;SUKL:{c.sukLord||"-"},&nbsp;NN:{c.planetNakName||"-"},&nbsp;SSS:{c.planetSubSubSubLord||"-"}
-                          </ReactTooltip>
-                        </Fragment>
-                    </td>
-                    
-                    {c.houseSignLord===undefined && <td>{"-"}</td>}
-                    {c.houseSignLord!==undefined && <td>{c.houseSignLord.substring(0,2)}</td>}
-                    {c.dhasaLord===undefined && <td className="highlight">{"-"}</td>}
-                    {c.dhasaLord!==undefined && <td className="highlight">{c.dhasaLord.substring(0,2)}</td>}
-                    {c.bukthiLord ===undefined&& <td>{"-"}</td>}
-                    {c.bukthiLord !==undefined&& <td className="highlight">{c.bukthiLord.substring(0,2)}</td>}
-                    {c.antharaLord ===undefined&&<td>{"-"}</td>}
-                    {c.antharaLord !==undefined&&<td>{c.antharaLord.substring(0,2)}</td>}
-                    <td>{c.degree||"-"}</td>
-                    {c.planet ===undefined &&<td>{c.planet||"-"}</td>}
-                    {c.planet !==undefined &&<td>{c.planet.substring(0,2)}</td>}
-                    {c.planetSignLord ===undefined &&<td>{c.planetSignLord||"-"}</td>}
-                    {c.planetSignLord !==undefined &&<td>{c.planetSignLord.substring(0,2)}</td>}
-                    {c.planetNakLord ===undefined&&<td>{"-"}</td>}
-                    {c.planetNakLord !==undefined&&<td>{c.planetNakLord.substring(0,2)}</td>}
-                    {c.planetSubLord ===undefined&&<td>{"-"}</td>}
-                    {c.planetSubLord !==undefined&&<td>{c.planetSubLord.substring(0,2)}</td>}
-                    {c.planetSubSubLord ===undefined&&<td>{"-"}</td>}
-                    {c.planetSubSubLord !==undefined&&<td>{c.planetSubSubLord.substring(0,2)}</td>}
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-
-          </div>
-        </div>
-        <div style={{ marginTop: 50 }}>
-          
-
-          <div className="pt_maincard table_container">
+          <div className="pt_maincard bava_table_container">
 
             <DisplayDhasa data={majorDasha} title="DasaData" handleClick={this.handleDasha} keyName="dasaData" />
             <DisplayDhasa data={subDasha} title="BukthiData" handleClick={this.handleDasha} keyName="bukthiData" />
@@ -299,7 +237,11 @@ class Home extends Component {
             <DisplayDhasa data={subDasha3} title="SukshmaData" handleClick={this.handleDasha} keyName="sukshmaData" />
             <DisplayDhasa data={subDasha4} title="AdhisukshmaData" handleClick={this.handleDasha} keyName="adhisukshmaData" />
           </div>
+            
+
+          </div>
         </div>
+        
       </div>
 
     )
