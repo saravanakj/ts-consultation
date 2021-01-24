1cf22b2b98b17c04409455b50a76e4d271490140
Saravana Kumar J <saravanakj@gmail.com>
2021-01-24 15:50:42 +0530

    degree text

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -198,7 +198,7 @@ class Home extends Component {
                   <td>DAL</td>
                   <td>BHL</td>
                   <td>ANL</td>
-                  <td>F.Degree</td>
+                  <td>Deg</td>
                   <td>PL</td>
                   <td>PSL</td>
                   <td>NL</td>
